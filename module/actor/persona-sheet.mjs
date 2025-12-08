98421e3d4d2f4740b5051faf449724daeb34871c
gribeiro239 <gribeiro239@gmail.com>
2025-12-07 21:56:15 -0300

    feat: create action event: click attribute

--- module/actor/persona-sheet.mjs
+++ module/actor/persona-sheet.mjs
@@ -22,20 +22,38 @@ export class PersonaSheet extends foundry.applications.api.HandlebarsApplication
     tag: "form", // Default is div in case we don't want to define a tag
     window: {
       icon: "fa fa-address-card"
+    },
+    actions: {
+      clickAttribute: this.onAttributeClick
     }
   };
 
   async _prepareContext(options) {
     // const context = await super._prepareContext() // is this still necessary?
-
+    
     context.actor = this.document;
     context.system = this.document.system;
     context.config = CONFIG.PC;
 
     return context;
   }
+
+  // find how to implement listeners
+  // use Handlebars and Action 
+
+  /**
+   * @param {PointerEvent} event - The originating click event
+   * @param {HTMLElement} target - the capturing HTML element which defined a [data-action]
+   */
+  static onAttributeClick(event, target) {
+    console.log("Attribute:", target.dataset.attribute);
+    event.preventDefault();
+  }
+
 }
 
+// how to make a roll
+// on click roll a d6 for clicked attribute
 
 /** Some reference, might be VERY WRONG */
 // export class ParsCrucisActorSheet extends api.HandlebarsApplicationMixin(
@@ -56,20 +74,3 @@ export class PersonaSheet extends foundry.applications.api.HandlebarsApplication
 //     data.PV = this.actor.PV; // dynamic PV
 //     return data;
 //   }
-
-//   activateListeners(html) {
-//     super.activateListeners(html);
-
-//     html.find(".increment").click((ev) => {
-//       const attr = ev.currentTarget.dataset.attr;
-//       const current = getProperty(this.actor.system, attr) || 0;
-//       this.actor.update({ [`system.${attr}`]: current + 1 });
-//     });
-
-//     html.find(".decrement").click((ev) => {
-//       const attr = ev.currentTarget.dataset.attr;
-//       const current = getProperty(this.actor.system, attr) || 0;
-//       if (current > 0) this.actor.update({ [`system.${attr}`]: current - 1 });
-//     });
-//   }
-// }
