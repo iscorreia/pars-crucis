e33c6983cf5ee67f44edbcd3ac73c060939f994c
gribeiro239 <gribeiro239@gmail.com>
2025-12-11 21:30:02 -0300

    feat: create ability item

---
+++ module/item/item-sheet.mjs
@@ -0,0 +1,88 @@
+const { api, sheets } = foundry.applications;
+import { PC } from "../config.mjs";
+
+export class ParsCrucisItemSheet extends api.HandlebarsApplicationMixin(
+  sheets.ItemSheetV2
+) {
+  static DEFAULT_OPTIONS = {
+    form: {
+      submitOnChange: true,
+    },
+    position: {
+      width: 600,
+      height: 440,
+    },
+    tag: "form",
+    window: {
+      icon: "fa fa-magic",
+    },
+  };
+
+  /** Defines where are the template PARTS */
+  static PARTS = {
+    header: {
+      template: "systems/pars-crucis/templates/item/parts/header.hbs",
+    },
+    tabs: {
+      template: "templates/generic/tab-navigation.hbs",
+    },
+    description: {
+      template: "systems/pars-crucis/templates/item/parts/description.hbs",
+    },
+  };
+
+  static TABS = {
+    primary: {
+      initial: "description",
+      tabs: [
+        {
+          id: "description",
+          label: "Descrição",
+        },
+      ],
+    },
+  };
+
+  getTitle() {
+    // Retorna apenas o nome do item, sem o tipo
+    return this.document.name || "Novo Item";
+  }
+
+  async _prepareContext() {
+    const context = {
+      item: this.document,
+      documento: this.document,
+      system: this.document.system,
+      config: CONFIG.PC,
+      tabs: this._prepareTabs("primary"),
+      itemType: this.document.type, // Add item type for template logic
+    };
+
+    return context;
+  }
+
+  async _preparePartContext(partId, context) {
+    switch (partId) {
+      case "description":
+        context.tab = context.tabs[partId];
+        break;
+      default:
+    }
+
+    return context;
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    if (!this.options.editable) return;
+
+    // Delete Self
+    html.find(".self-destruct").click((ev) => {
+      const id = $(ev.currentTarget).attr("self");
+      if (this.document.actor != null) {
+        this.document.actor.items.get(id)?.delete();
+      }
+    });
+  }
+}
