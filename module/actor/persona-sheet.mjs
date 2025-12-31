7064ce1cf860749ba794f89dc713437ea221a51a
iscorreia <ismaelavilacorreia@gmail.com>
2025-12-31 10:31:09 -0300

    style: passive tab hbs css

--- module/actor/persona-sheet.mjs
+++ module/actor/persona-sheet.mjs
@@ -68,7 +68,7 @@ export class PersonaSheet extends api.HandlebarsApplicationMixin(
 
   static TABS = {
     primary: {
-      initial: "gear", // Change to simplify testing, once done set back to skills
+      initial: "passives", // Change to simplify testing, once done set back to skills
       tabs: [
         { id: "skills", label: "PC.tabs.skills" },
         { id: "abilities", label: "PC.tabs.abilities" },
